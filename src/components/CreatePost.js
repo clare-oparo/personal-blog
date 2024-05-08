c9c96fd0805e0c96413747ed17eeb2b5fcba827c
Clare <clare.oparo@student.moringaschool.com>
2024-05-08 15:52:55 +0300

    added create post

---
+++ src/components/CreatePost.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function CreatePost() {
+  return (
+    <div>
+      <h1>Create or Edit a Post</h1>
+      <form>
+        <input type="text" placeholder="Title" />
+        <textarea placeholder="Content"></textarea>
+        <button type="submit">Save Post</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreatePost;
