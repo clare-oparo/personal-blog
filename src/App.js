49054c4d8ef0852432e1f734beeecdec24010fb5
Clare <clare.oparo@student.moringaschool.com>
2024-05-08 17:52:19 +0300

    cookie set for fb rehearsal

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Home from './components/Home';
 import Post from './components/Post';
 import CreatePost from './components/CreatePost';
+import Cookies from 'js-cookie';
 
 
 function App() {
   const [auth, setAuth] = useState(false);
+
+  useEffect(() => {
+    const checkReferrerAndSetCookie = () => {
+      const simulateReferrer = "https://www.facebook.com";
+      if (simulateReferrer.includes("facebook.com")) {
+        Cookies.set('referredBy', 'Facebook', { expires: 7 }); //cookie expires in 7 days
+        console.log('Cookie set for Facebook referral');
+      } 
+    };
+
+    checkReferrerAndSetCookie();
+  }, []);
 
   return (
     <Router>
