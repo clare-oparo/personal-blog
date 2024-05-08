f390020acee521720ac20027dc5ebacaf23a0477
Clare <clare.oparo@student.moringaschool.com>
2024-05-08 15:59:56 +0300

    fetch posts in home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch('posts.json')
+      .then(response => response.json())
+      .then(data => setPosts(data))
+      .catch(error => console.error('Error loading posts:', error));
+  }, []);
+
   return (
     <div>
       <h1>Blog Posts</h1>
-      {/* Link to a specific post by ID */}
-      <Link to="/post/1">Read Post</Link>
+      {posts.map(post => (
+        <div key={post.id}>
+          <h2>{post.title}</h2>
+          {/* Link to the specific post */}
+          <Link to={`/post/${post.id}`}>Read More</Link>
+        </div>
+      ))}
       {/* Link to create a new post */}
       <Link to="/create-post">Create Post</Link>
     </div>
