44ab8fec50bbd8f5561857ab1e64b45fc6635224
Clare <clare.oparo@student.moringaschool.com>
2024-05-08 15:55:19 +0300

    updated App

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './Login';
+import Home from './Home';
+import Post from './Post';
+import CreatePost from './CreatePost';
 
-function CreatePost() {
+function App() {
+  const [auth, setAuth] = useState(false);
+
   return (
-    <div>
-      <h1>Create or Edit a Post</h1>
-      <form>
-        <input type="text" placeholder="Title" />
-        <textarea placeholder="Content"></textarea>
-        <button type="submit">Save Post</button>
-      </form>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login setAuth={setAuth} />} />
+        <Route path="/" element={auth ? <Home /> : <Login setAuth={setAuth} />} />
+        <Route path="/post/:id" element={auth ? <Post /> : <Login setAuth={setAuth} />} />
+        <Route path="/create-post" element={auth ? <CreatePost /> : <Login setAuth={setAuth} />} />
+      </Routes>
+    </Router>
   );
 }
 
-export default CreatePost;
+export default App;
