455f7839d9d3021727d06e1a5e3d589c8d228fa5
Clare <clare.oparo@student.moringaschool.com>
2024-05-08 16:01:23 +0300

    fetch post based on id

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 function Post() {
   const { id } = useParams();
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {
+    fetch('posts.json')
+      .then(response => response.json())
+      .then(data => {
+        const singlePost = data.find(p => p.id === parseInt(id));
+        setPost(singlePost);
+      })
+      .catch(error => console.error('Error loading post:', error));
+  }, [id]);
+
+  if (!post) return <div>Loading...</div>;
 
   return (
     <div>
-      <h1>Post {id}</h1>
-      <p>This is the content of post {id}.</p>
+      <h1>{post.title}</h1>
+      <p>{post.content}</p>
     </div>
   );
 }
