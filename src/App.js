40b4027772b9de60cbb80254e5039a8fe1dbf1ed
Clare <clare.oparo@student.moringaschool.com>
2024-05-08 16:06:39 +0300

    updated create post

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './Login';
+import Home from './Home';
+import Post from './Post';
+import CreatePost from './CreatePost';
 
 function App() {
+  const [auth, setAuth] = useState(false);
+
   return (
-    <div>we out here building 😎</div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login setAuth={setAuth} />} />
+        <Route path="/" element={auth ? <Home /> : <Login setAuth={setAuth} />} />
+        <Route path="/post/:id" element={auth ? <Post /> : <Login setAuth={setAuth} />} />
+        <Route path="/create-post" element={auth ? <CreatePost /> : <Login setAuth={setAuth} />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,23 +1,16 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './Login';
-import Home from './Home';
-import Post from './Post';
-import CreatePost from './CreatePost';
+import React from 'react';
 
-function App() {
-  const [auth, setAuth] = useState(false);
-
+function CreatePost() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login setAuth={setAuth} />} />
-        <Route path="/" element={auth ? <Home /> : <Login setAuth={setAuth} />} />
-        <Route path="/post/:id" element={auth ? <Post /> : <Login setAuth={setAuth} />} />
-        <Route path="/create-post" element={auth ? <CreatePost /> : <Login setAuth={setAuth} />} />
-      </Routes>
-    </Router>
+    <div>
+      <h1>Create or Edit a Post</h1>
+      <form>
+        <input type="text" placeholder="Title" />
+        <textarea placeholder="Content"></textarea>
+        <button type="submit">Save Post</button>
+      </form>
+    </div>
   );
 }
 
-export default App;
+export default CreatePost;
